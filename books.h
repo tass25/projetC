97d387d1d4cf916e5d41ab9fb48727b5f8ec9b61
tass <laboutesse@gmail.com>
2023-12-07 12:14:55 +0100

    afficher le livre ( avec son code )

--- books.h
+++ books.h
@@ -5,11 +5,12 @@
 
 #include "utility.h"
 
-void initialiser_liste_Livre(Liste_Livre *L);
-void saisir_livre(Livre *L);
-void Ajouter_Livre_list(Liste_Livre *l, Livre L);
-Noeud *chercher_Liste(Liste_Livre l, int code);
-void Afficher_Livre(int code);
+void initialiser_liste_Livre(Liste_Livre *L);           //done
+void saisir_livre(Livre *L);                           //done
+void Ajouter_Livre_list(Liste_Livre *l, Livre L);     //done
+Noeud *chercher_Liste(Liste_Livre l, int code);      //done
+void Afficher_Livre(int code);                      // done
+int chercher_Livre_Dans_Fichier(Livre *L, int code); //done
 int Chercher_Livre(Liste_Livre Disponible, Liste_Livre Emprunte, Liste_Livre En_Reparation, int x);
 Noeud *Recherche_livre(Liste_Livre Disponible, Liste_Livre Emprunte, Liste_Livre En_Reparation, int code);
 void Modifier_Annee_publication(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, int xx);
@@ -17,7 +18,7 @@ void Modifier_Titre(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre 
 void Modifier_Auteur(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, int xx);
 void remplire_liste_Disponible(Liste_Livre *Disponible, int n);
 void Afficher_Livre_Par_Annee(Liste_Livre Disponible, Liste_Livre Emprunte, Liste_Livre En_Reparation, int anne);
-void Supprimer_Livre(Liste_Livre *l, int x);
+void Supprimer_Livre(Liste_Livre *l, int x);           //done
 
 
 #endif //PROJET_C_BOOKS_H
