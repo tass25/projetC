f5f5fa80446858e114f3e51cc9cded75ad0ce73b
tass <laboutesse@gmail.com>
2023-12-20 00:43:17 +0100

    :) :) :) hedhy mch 3icha lha9

--- abonne.h
+++ abonne.h
@@ -14,11 +14,11 @@ void Afficher_liste_Abonne(Liste_Abonne LAB);
 Noeud1 *Chercher_Abonne(Liste_Abonne LAB, int ident);
 void Modifier_Abonne(Liste_Abonne *LAB, int x);
 void Supprimer_Abonne(Liste_Abonne *LAB, int x);
-void handleBookBorrowing(Liste_Livre *Disponible, Liste_Livre *Emprunte, int bookCode);
-void Emprunter_Livre(Liste_Abonne LAB, Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, Abonne *A, int bookCode, int ident);
-void sauvegarderEtatDesLivres(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation);
-void Envoyer_Livre_Reparation(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, Abonne *A, int x);
-void Rendre_Livre(Liste_Abonne LAB, Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, Abonne *A, int x, int ident);
+void handleBookBorrowing(Liste_Livre *Disponible, int bookCode);
+void Emprunter_Livre(Liste_Abonne LAB, Liste_Livre *Disponible, Liste_Livre *En_Reparation, Abonne *A, int bookCode, int ident);
+void sauvegarderEtatDesLivres(Liste_Livre *Disponible, Liste_Livre *En_Reparation);
+void Envoyer_Livre_Reparation(Liste_Livre *Disponible, Liste_Livre *En_Reparation, Abonne *A, int x);
+void Rendre_Livre(Liste_Abonne LAB, Liste_Livre *Disponible, Liste_Livre *En_Reparation, Abonne *A, int x, int ident);
 void chargerAbonnesDepuisFichier(Liste_Abonne *LAB);
 
 
--- books.h
+++ books.h
@@ -8,17 +8,17 @@
 void initialiser_liste_Livre(Liste_Livre *L);
 void saisir_livre(Livre *L);
 void Ajouter_Livre_list(Liste_Livre *l, Livre L);
-void chargerLivresDepuisFichier(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation);
+void chargerLivresDepuisFichier(Liste_Livre *Disponible, Liste_Livre *En_Reparation);
 void Supprimer_Livre(Liste_Livre *l, int x);
 Noeud *chercher_Liste(Liste_Livre l, int code);
 int chercher_Livre_Dans_Fichier(Livre *L, int code);
 void Afficher_Livre(int code);
-int Chercher_Livre(Liste_Livre Disponible, Liste_Livre Emprunte, Liste_Livre En_Reparation, int code);
-Noeud *Recherche_livre(Liste_Livre Disponible, Liste_Livre Emprunte, Liste_Livre En_Reparation, int code);
-void Modifier_Annee_publication(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, int code);
-void Modifier_Titre(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, int code);
-void Modifier_Auteur(Liste_Livre *Disponible, Liste_Livre *Emprunte, Liste_Livre *En_Reparation, int code);
+int Chercher_Livre(Liste_Livre Disponible, Liste_Livre En_Reparation, int code);
+Noeud *Recherche_livre(Liste_Livre Disponible, Liste_Livre En_Reparation, int code);
+void Modifier_Annee_publication(Liste_Livre *Disponible, Liste_Livre *En_Reparation, int code);
+void Modifier_Titre(Liste_Livre *Disponible, Liste_Livre *En_Reparation, int code);
+void Modifier_Auteur(Liste_Livre *Disponible, Liste_Livre *En_Reparation, int code);
 void remplire_liste_Disponible(Liste_Livre *Disponible, int n);
-void Afficher_Livres_Par_Annee(Liste_Livre Disponible, Liste_Livre Emprunte, Liste_Livre En_Reparation, int annee);
+void Afficher_Livres_Par_Annee(Liste_Livre Disponible, Liste_Livre En_Reparation, int annee);
 void sauvegarderLivresDansFichier(Liste_Livre *liste);
 #endif //PROJET_C_BOOKS_H
