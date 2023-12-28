af78a66448d69ee459410c060409467f63e2b733
tass <laboutesse@gmail.com>
2023-12-28 04:26:22 +0100

    clawzed

--- utility.h
+++ utility.h
@@ -56,22 +56,20 @@ typedef struct {
 } Liste_Abonne;
 
 
-void hidecursor();
-void h_line(int x,int y, int l);
-void textcolor(int color);
-void gotoxy(int x,int y);
-void delay(int mseconds);
-void splash_s(int x, int y, int l, int h);
-void box(int a, int c,int l,int h);
-void loader(int x,int y,int z);
-int menu_selector(int x, int y1, int y2, char (*items)[100]);
-int validTitle(char *title);
-int validCode(int code);
-int validName(char *name);
-int validYear(int year);
-int validState(int state);
-int selector(int x, int y1, int y2, char (*items)[100]);
+void hidecursor();//
+void h_line(int x,int y, int l);//
+void textcolor(int color);//
+void gotoxy(int x,int y);//
+void delay(int mseconds);//
+void splash_s(int x, int y, int l, int h);//
+void box(int a, int c,int l,int h);//
+void loader(int x,int y,int z);//
+int menu_selector(int x, int y1, int y2, char (*items)[100]);//
+int validTitle(char *title);//
+int validCode(int code);//
+int validName(char *name);//
+int validYear(int year);//
+int validState(int state);//
+int selector(int x, int y1, int y2, char (*items)[100]);//
 
-//void sauvegarderAbonnesDansFichier(Liste_Abonne *LAB);
-//void chargerAbonnesDepuisFichier(Liste_Abonne *LAB);
 #endif //PROJET_C_UTILITY_H
